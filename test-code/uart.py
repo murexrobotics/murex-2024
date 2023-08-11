bf0829ca8c061f306b1efb41e35cb3af7238413a
L0ad1n6 <altan.mehmet.unver@gmail.com>
2023-08-11 18:56:56 +0300

    test(uart): Python uart test code for cm4

---
+++ test-code/uart.py
@@ -0,0 +1,19 @@
+import time
+import serial
+
+print("Starting UART")
+ser = serial.Serial(
+        port='/dev/ttySO', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+)
+
+print("Sending Messages")
+messages = [0x00, 0x01, 0x02, 0x03, 0x04]
+for msg in messages:
+    print(f"<<< {msg}")
+    ser.write([msg])
+    time.sleep(1)
+    print(ser.read(size=1))
