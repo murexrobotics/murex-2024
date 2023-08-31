4598157d9c0c5fafa6f3b25d42c791c8d45bc834
L0ad1n6 <altan.mehmet.unver@gmail.com>
2023-08-31 14:01:54 +0300

    feat: Uart test code

--- test-code/uart.py
+++ test-code/uart.py
@@ -3,7 +3,7 @@ import serial
 
 print("Starting UART")
 ser = serial.Serial(
-        port='/dev/ttySO', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/serial0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
@@ -14,6 +14,6 @@ print("Sending Messages")
 messages = [0x00, 0x01, 0x02, 0x03, 0x04]
 for msg in messages:
     print(f"<<< {msg}")
-    ser.write([msg])
+    ser.write(b"Hello world")
     time.sleep(1)
     print(ser.read(size=1))
