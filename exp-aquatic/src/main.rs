264f2bde86a759374abd16abdc99c131bf60e127
Altan Unver <altan.mehmet.unver@gmail.com>
2023-09-16 15:27:58 -0400

    feat: async task executor beta

--- exp-aquatic/src/main.rs
+++ exp-aquatic/src/main.rs
@@ -1,10 +1,19 @@
 #![allow(dead_code, unused_must_use)]
 #![feature(async_fn_in_trait)]
 
-use tokio;
 // mod input;
 // mod error;
 mod actions;
+mod executor;
 
-fn main() {
+use tokio;
+// use actions::Executor;
+use executor::Exec;
+
+
+#[tokio::main]
+async fn main() {
+    let mut exec = Exec::new().await;
+    exec.run(); // Never ends
+
 }
