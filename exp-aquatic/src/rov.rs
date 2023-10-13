fa38a687f90466ad955206ff9da8362372f0db0b
Altan Unver <altan.mehmet.unver@gmail.com>
2023-10-13 01:59:03 -0400

    feat(exp-aquatic): MASCP thruster control done, needs testing

--- exp-aquatic/src/rov.rs
+++ exp-aquatic/src/rov.rs
@@ -1,5 +1,10 @@
 use linux_embedded_hal::{I2cdev, Spidev};
-use rppal::uart::Uart;
+use rppal::uart::{Uart, Parity};
+use anyhow::{Result, bail};
+use log::{trace, info, warn, error, debug};
+
+
+use std::time::Duration;
 
 pub struct Rov {
     i2c: I2cdev,
@@ -7,7 +12,7 @@ pub struct Rov {
     uart: Uart,
 
     pub thrusts: (f32, f32, f32, f32, f32, f32), // (front, back, left, right, top, bottom)
-    
+
     pub acceleration: (f32, f32, f32), // (x, y, z)
     pub velocity: (f32, f32, f32), // (x, y, z)
     pub position: (f32, f32, f32), // (x, y, z)
@@ -29,6 +34,17 @@ pub struct Rov {
 
 impl Rov {
     pub fn new() -> Self {
+        // TODO: Initialize I2C, SPI, and UART
+        // TODO: Remove .unwrap() calls and do propper error handling
+        let mut uart = Uart::new(
+            115_200, 
+            Parity::None, 
+            8, 
+            1)
+            .unwrap();
+
+        uart.set_read_mode(1, Duration::default()).unwrap();
+        uart.set_write_mode(true).unwrap();
         todo!()
     }
 
@@ -72,7 +88,62 @@ impl Rov {
         todo!()
     }
 
-    pub(crate) fn set_thruster(&self, thruster: usize, thrust: f32) {
-        todo!()
+    /// Sets the thrust of a given thruster using MASCP
+    /// 
+    /// # Arguments
+    ///    * `thruster` - The thruster to set the thrust of
+    ///    * `thrust` - The thrust to set the thruster to (-1.0 - 1.0)
+    /// 
+    /// # Returns
+    ///   * `Result<()>` - Ok if the thruster was set successfully, otherwise an error
+    pub(crate) fn set_thruster(&mut self, thruster: usize, thrust: f32) -> Result<()>  {
+        // TODO: Clarify structure for M[3] and M[4]
+        // MASCP Thruster Write
+        // M[0] = MASCP_THRUSTER_WRITE_INSTRUCTION
+        // M[1] = THRUSTER_SELECT = 0x00 - 0x05 (Thruster 0 - 5)
+        // M[2] = POSITIVE_THRUST = 0x00 - 0xFF (Thrust 0 - 255)
+        // M[3] = NEGATIVE_THRUST = 0x00 - 0xFF (Thrust 0 - 255)
+        // TODO: M[3] = INTERPOLATION_TIME
+        // TODO: M[4] = INTERPOLATION_FUNCTION_SELECT
+        // M[5-8] = 0xFF (Padding, later possibly checksum)
+
+        // Check if a valid thruster is being selected
+        if thruster > 5 || thruster < 0 {
+            warn!(target: "thruster_events", "Thruster `{}` is out of range", thruster);
+            bail!("Invalid thruster selected");
+        }
+        
+        // clamp thrust value between 0 and 1, then scale to 0 - 255
+        let thrust = thrust.max(-1.0).min(1.0) * 255.0;
+        trace!(target: "thruster_events", "Setting thruster {} to {}", thruster, thrust);
+        
+        // Send thruster command
+        let msg = [
+            0x00, 
+            thruster as u8, 
+            thrust as u8,           // Positive thrust
+            (-thrust) as u8,        // Negative thrust
+            0x00,                   // TBD interpolation time
+            0x00,                   // TBD interpolation function
+            0xFF,                   // Padding
+            0xFF,                   // Padding
+        ];
+        self.uart.write(&msg);
+        trace!(target: "thruster_events", "Sending MASCP thruster command payload");
+
+        // Validate thruster
+        let mut buffer = [0u8; 8];
+        // buffer [0] should be MASCP Thruster Write Response
+        // buffer [1-5] should be equivalent to the message
+        // buffer [6-7] should be 0x00
+        // TODO: Do additional checks as necessary, even these check is probably overkill
+        self.uart.read(&mut buffer);
+        if buffer[0] != 0x01 || // MASCP Thruster Write Response
+            buffer[6] != 0x00 || // ESC Padding is 0x00
+            buffer[7] != 0x00   // ESC Padding is 0x00
+        {
+            bail!("Thruster write failed")
+        }
+        Ok(())
     }
 }
