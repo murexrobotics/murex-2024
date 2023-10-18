db80dbea693d7a4ebce373baba98d4cb8cac4eaa
Altan Unver <altan.mehmet.unver@gmail.com>
2023-10-17 21:02:19 -0400

    fix(exp-aquatic): Update Action trait so that it consumes `self`

--- exp-aquatic/src/actions/movement.rs
+++ exp-aquatic/src/actions/movement.rs
@@ -3,70 +3,70 @@ use crate::rov::Rov;
 
 struct MoveToHeading(f32);
 impl Action for MoveToHeading {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct MoveToDepth(f32);
 impl Action for MoveToDepth {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct MoveToPosition(f32, f32, f32);
 impl Action for MoveToPosition {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct MoveToRelativeHeading(f32);
 impl Action for MoveToRelativeHeading {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct MoveToRelativeDepth(f32);
 impl Action for MoveToRelativeDepth {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct MoveToRelativePosition(f32, f32, f32);
 impl Action for MoveToRelativePosition {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct ThrusterControl(usize, f32);
 impl Action for ThrusterControl {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct Resurface();
 impl Action for Resurface {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct EmergencySurface();
 impl Action for EmergencySurface {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct EmergencyStop();
 impl Action for EmergencyStop {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         for i in 0..6 {
             rov.set_thruster(i, 0f32);
         }
@@ -75,7 +75,7 @@ impl Action for EmergencyStop {
 
 struct ToggleStabilityControl(bool);
 impl Action for ToggleStabilityControl {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
--- exp-aquatic/src/actions/system.rs
+++ exp-aquatic/src/actions/system.rs
@@ -3,34 +3,34 @@ use crate::rov::Rov;
 
 struct Reboot();
 impl Action for Reboot {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct Shutdown();
 impl Action for Shutdown {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct SystemReport();
 impl Action for SystemReport {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
     }
 }
 
 struct LeakReport();
 impl Action for LeakReport {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
 
 struct CallibrateSensors();
 impl Action for CallibrateSensors {
-    fn exec(&self, rov: &mut Rov) {
+    fn exec(self, rov: &mut Rov) {
         todo!()
     }
 }
