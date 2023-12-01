b5fb5ca15209f3187d403e85b3bd887364a0ed58
Altan Mehmet Ünver <77926625+L0ad1n6@users.noreply.github.com>
2023-12-01 01:06:28 -0500

    feat(exp-aquatic): wip system event loop

--- exp-aquatic/src/system.rs
+++ exp-aquatic/src/system.rs
@@ -7,6 +7,8 @@ use nalgebra::{Vector3, Quaternion};
 
 use crate::telemetry::TelemetryPacket;
 
+const SAMPLE_RATE: f32 = 0.1;
+const BETA: f32 = 0.1;
 
 #[derive(Debug)]
 pub enum SystemCommand {
@@ -26,10 +28,33 @@ pub struct System {
 
 impl System {
     pub fn start(tel_receiver: Receiver<TelemetryPacket>) -> System {
-        // TODO: Init MPSC channels
-        // TODO: calculate AHRS + Position when telemetry packet is received
-        // TODO: Task execution event loop
-        todo!()
+        let (main_sender, main_receiver) = std::sync::mpsc::channel();
+        let handle = std::thread::spawn(move || {
+            let mut ahrs = Madgwick::new(SAMPLE_RATE, BETA);
+            
+            
+            loop {
+                let tel = tel_receiver.recv().expect("Failed to receive telemetry packet");
+
+                let accel = Vector3::new(tel.acceleration.0, tel.acceleration.1, tel.acceleration.2);
+                // Gyro data needs to be in radians, fix if necessarry
+                let gyro = Vector3::new(tel.angular_velocity.0, tel.angular_velocity.1, tel.angular_velocity.2);
+                // Magnetometer data is in gauss, TODO: convert to right units, unsure if it is Tesla's or not
+                let mag = Vector3::new(tel.magnetic_field.0 as f32, tel.magnetic_field.1 as f32, tel.magnetic_field.2 as f32);
+
+                let quat = ahrs.update(
+                    &gyro,
+                    &accel,
+                    &mag,
+                ).expect("Failed to update AHRS filter");
+
+                let (roll, pitch, yaw) = quat.euler_angles();
+            }
+        });
+        System {
+            handle: Some(handle),
+            main_sender,
+        }
     }
 }
 
