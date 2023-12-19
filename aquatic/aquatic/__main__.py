e098ecdf00696b18fccf141f0c09f6a7ef626c4c
anikas26 <ajsivarasa@gmail.com>
2023-12-19 17:58:50 -0500

    Feat: Updating thruster values from topside info

--- aquatic/aquatic/__main__.py
+++ aquatic/aquatic/__main__.py
@@ -1,20 +1,41 @@
 import time
+import json
 import thrusters, bme680, camera, arm, pixel, i2c
-import udpsocket
+from udpsocket import socket
 
-websocket = udpsocket.socket
+UPDATE_TIME = 0.01
+
+thrusterslist = []
+#Initializing thrusters, 0 is a placeholder for addresses
+t1 = thrusters.Thruster("t1", 0)
+t2 = thrusters.Thruster("t2", 0)
+t3 = thrusters.Thruster("t3", 0)
+t4 = thrusters.Thruster("t4", 0)
+t5 = thrusters.Thruster("t5", 0)
+t6 = thrusters.Thruster("t6", 0)
+
+thrusterslist.append(t1, t2, t3, t4, t5, t6)
+
 telemetrydict = {}
-components = [thrusters, bme680, camera, arm] #can be added to
+
+components = [bme680, camera, arm] #can be added to
+for thruster in thrusterslist:
+    components.append(thruster)
+
+#for getting state info from topside and setting thruster values
+def my_event_handler(message):
+    msg = json.loads(message.decode())
+    for thruster in thrusterslist:
+        thruster.set_throttle(msg[thruster.name]["throttle"], True)
+
 
 def main():
     while True:
         for component in components:
-            telemetrydict.update(component.telemetry())
-        websocket.send(telemetrydict)
-        time.sleep(0.01)
-        '''
-        TODO
-        - Initialize thrusters
-        - Receive information from topside
-        - Update the state
-        '''
+            if (hasattr(component, 'update') and component.isfunction(component.update)):
+                component.update()
+            if (hasattr(component, 'telemetry') and component.isfunction(component.telemetry)):
+                telemetrydict.update(component.telemetry())
+        socket.send(telemetrydict)
+        time.sleep(UPDATE_TIME)
+        socket.on_message_received += my_event_handler
--- aquatic/aquatic/thrusters.py
+++ aquatic/aquatic/thrusters.py
@@ -1,8 +1,49 @@
+import serial
+
+MASCP_FORMAT = "[{address}, {thrust_magnitude}]"
+THRUSTER_MAX = 255
+THRUSTER_NEUTRAL = 127
+THRUSTER_MIN = 0
+
 thrusters = []
 
-#Temporary until thruster code is written:
-for thruster in thrusters:
-    thruster.throttle = 0
+class Thruster:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.set_throttle(THRUSTER_NEUTRAL)
+
+    def __del__(self):
+        self.set_throttle(THRUSTER_NEUTRAL)
+
+    def update(self):
+        pass
+
+    def set_throttle(self, throttle, from_topside=False):
+
+        if from_topside:
+            # convert from value between -1 and 1 to 0 and 255
+            throttle = (throttle + 1) * THRUSTER_NEUTRAL
+
+        # Ensure throttle is within bounds
+        throttle = max(THRUSTER_MIN, min(THRUSTER_MAX, throttle))
+
+        self.throttle = throttle
+        MASCP_FORMAT.format(address=self.address, thrust_magnitude=self.throttle)
+
+    def get_throttle(self):
+        return self.throttle
+    
+    def telemetry(self):
+        return {
+            "name": self.name,
+            "port": self.port,
+            "throttle": self.throttle
+        }
+
+def update():
+    for thruster in thrusters:
+        thruster.update()
 
 def telemetry():
     json = {
@@ -10,6 +51,6 @@ def telemetry():
     }
 
     for thruster in thrusters:
-        json["thrusters"][thruster.name] = thruster.throttle
+        json["thrusters"][thruster.name] = thruster.telemetry()
 
     return json
